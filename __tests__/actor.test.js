3be5e0eada575ddaaea70f205aac592a5ba1a77c
Tali Avni <tali@aristo7.com>
2020-01-06 13:40:32 -0800

    created actor model, routes, tests. starting on films

---
+++ __tests__/actor.test.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+
+const request = require('supertest');
+const app = require('../lib/app');
+const connect = require('../lib/utils/connect');
+const mongoose = require('mongoose');
+const Actor = require('../lib/models/Actor');
+
+describe('actor routes', () => {
+  beforeAll(() => {
+    connect();
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  let actor;
+
+  beforeEach(async() => {
+    actor = await Actor.create({
+      name: 'Actor Name',
+      dob: 'January 1, 1990',
+      pob: 'Place'
+    });
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+
+  it('creates a studio', () => {
+    return request(app)
+      .post('/api/v1/studios')
+      .send({
+        name: 'Actor Name',
+        dob: 'January 1, 1990',
+        pob: 'Place'
+      });
+  });
+});
